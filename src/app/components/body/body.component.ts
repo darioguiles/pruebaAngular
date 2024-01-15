013a86ad619791728e515a948d424bd8111b3115
darioguiles <dario.guilescuesta@hotmail.com>
2024-01-15 11:47:45 +0100

    a

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,19 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  standalone: true,
+  imports: [NgIf,NgFor], //Los imports del HTML van aquí pero si son de clase solo arriba
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.css'
+})
+export class BodyComponent {
+  mostrar:boolean= true;
+
+  lista:String[] = ["Dario","Daniel","Fernando","Lucas","Miguel"];
+
+mostrarOcultar(){
+  this.mostrar = !this.mostrar;
+}
+}
